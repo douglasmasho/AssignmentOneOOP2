8c950f16e54a9d6a86a5b688174e52477b10e32e
Gabriel-Ndimulunde <111272626+Gabriel-Ndimulunde@users.noreply.github.com>
2022-08-18 12:12:47 +0200

    Updated RentalsDept

--- src/RentalsDept.java
+++ src/RentalsDept.java
@@ -1,76 +1,104 @@
 import java.util.ArrayList;
 
-public class RentalsDept {
+public class RentalsDept 
+{
     private ArrayList<Vehicle> vehicles;
     private ArrayList<CustomerSlip> slips;
 
-
-    public RentalsDept() {
+    //Public Constructor for RentalsDept Class
+    public RentalsDept() 
+    {
         this.vehicles = new ArrayList<>();
         this.slips = new ArrayList<>();
     }
-
-    public void PrintAllVehicles(){
-        for(int i = 0; i < vehicles.size(); i++){
+    
+    //Prints out all vehicles in Array 
+    public void PrintAllVehicles()
+    {
+        for(int i = 0; i < vehicles.size(); i++)
+        {
             System.out.println(vehicles.get(i).getModel() + " " + vehicles.get(i).getVin());
         }
     }
-
-    public void AddVehicle(Vehicle vehicle) {
+    
+    //Add a Vehicle to Array
+    public void AddVehicle(Vehicle vehicle) 
+    {
         vehicles.add(vehicle);
     }
 
-    public Vehicle GetVehicle(String vin) {
+    //Method gets Vehicle that corresponds to inserted parameter vin number
+    public Vehicle GetVehicle(String vin) 
+    {
         System.out.println("Vin entered" + " " + vin);
-        for(int i = 0; i < vehicles.size(); i++) {
-            if(vehicles.get(i).getVin().equals(vin)) {
+        for(int i = 0; i < vehicles.size(); i++) 
+        {
+            if(vehicles.get(i).getVin().equals(vin)) 
+            {
                 return vehicles.get(i);
             }
         }
         return null;
     }
 
-    public void AddSlip(CustomerSlip slip) {
+    //Method Adds a Slip to Array of Slips
+    public void AddSlip(CustomerSlip slip) 
+    {
         slips.add(slip);
     }
 
-    public int HowManySedansOfColor(String color) {
+    //Method returns number of Sedans having specific color from Array
+    public int HowManySedansOfColor(String color) 
+    {
         int count = 0;
-        for(int i = 0; i < vehicles.size(); i++) {
-            if(vehicles.get(i) instanceof Sedan && vehicles.get(i).getColor().equals(color)) {
+        for(int i = 0; i < vehicles.size(); i++) 
+        {
+            if(vehicles.get(i) instanceof Sedan && vehicles.get(i).getColor().equals(color)) 
+            {
                 count++;
             }
         }
         return count;
     }
-    public int HowManyBakkiesOfColor(String color) {
+    
+    //Method returns number of Bakkies having specific color from Array
+    public int HowManyBakkiesOfColor(String color) 
+    {
         int count = 0;
-        for(int i = 0; i < vehicles.size(); i++) {
-            if(vehicles.get(i) instanceof Bakkie && vehicles.get(i).getColor().equals(color)) {
+        for(int i = 0; i < vehicles.size(); i++) 
+        {
+            if(vehicles.get(i) instanceof Bakkie && vehicles.get(i).getColor().equals(color))
+            {
                 count++;
             }
         }
         return count;
     }
-    public int HowManyBakkiesinYear(int year) {
+    
+    //Method returns number of Bakkies rented out in specific year
+    public int HowManyBakkiesinYear(int year) 
+    {
         int count = 0;
-        for(int i = 0; i < vehicles.size(); i++) {
-            if(vehicles.get(i) instanceof Bakkie && vehicles.get(i).getYear() == year) {
+        for(int i = 0; i < vehicles.size(); i++) 
+        {
+            if(vehicles.get(i) instanceof Bakkie && vehicles.get(i).getYear() == year) 
+            {
                 count++;
             }
         }
         return count;
     }
-
-
-
-
-    public String GetBiggestBakkie() {
+    
+    public String GetBiggestBakkie() 
+    {
         //since the first element in the array can be either a bakkie or a sedan, we will have to use the smallest number possible instead of using the first vehicle's volume
         String biggestBakkie = "No Bakkie found";
         double biggestBakkieVolume = Double.NEGATIVE_INFINITY;
-        for(int i = 0; i < vehicles.size(); i++) {
-            if(vehicles.get(i) instanceof Bakkie && vehicles.get(i).getVolume() > biggestBakkieVolume) {
+        
+        for(int i = 0; i < vehicles.size(); i++) 
+        {
+            if((vehicles.get(i) instanceof Bakkie) && (vehicles.get(i).getVolume() > biggestBakkieVolume)) 
+            {
                 biggestBakkie = vehicles.get(i).getModel() + " " +vehicles.get(i).getVin();
                 biggestBakkieVolume = vehicles.get(i).getVolume();
             }
@@ -78,12 +106,15 @@ public class RentalsDept {
         return biggestBakkie;
     }
 
-    public String GetBiggestBoot() {
+    public String GetBiggestBoot() 
+    {
         //since the first element in the array can be either a bakkie or a sedan, we will have to use the smallest number possible instead of using the first vehicle's volume
         String biggestBoot = "No Sedan found";
         double biggestBootVolume = Double.NEGATIVE_INFINITY;
-        for(int i = 0; i < vehicles.size(); i++) {
-            if(vehicles.get(i) instanceof Sedan && vehicles.get(i).getVolume() > biggestBootVolume) {
+        for(int i = 0; i < vehicles.size(); i++) 
+        {
+            if((vehicles.get(i) instanceof Sedan) && (vehicles.get(i).getVolume() > biggestBootVolume)) 
+            {
                 biggestBoot = vehicles.get(i).getModel() + " " +vehicles.get(i).getVin();
                 biggestBootVolume = vehicles.get(i).getVolume();
             }
@@ -91,17 +122,23 @@ public class RentalsDept {
         return biggestBoot;
     }
 
-    public double GetAverageKilometers() {
+    //Method Calculates the Average of kilometers travelled by Vehicles 
+    public double GetAverageKilometers() 
+    {
         double sum = 0;
-        for(int i = 0; i < slips.size(); i++) {
+        for(int i = 0; i < slips.size(); i++) 
+        {
             sum += slips.get(i).GetKilometres();
         }
         return sum / slips.size();
     }
 
-    public double GetAverageHours() {
+    //Calculates the Average of Hours that was driven by Vehicles
+    public double GetAverageHours() 
+    {
         double sum = 0;
-        for(int i =0; i<slips.size(); i++) {
+        for(int i =0; i<slips.size(); i++) 
+        {
             sum+=slips.get(i).GetHours();
         }
         return sum / slips.size();
