9b914fb19be5075b09df00342db2d763779c0ef3
Gabriel-Ndimulunde <111272626+Gabriel-Ndimulunde@users.noreply.github.com>
2022-08-18 11:54:09 +0200

    Updated Bakkie Class

--- src/Bakkie.java
+++ src/Bakkie.java
@@ -1,12 +1,15 @@
-public class Bakkie extends Vehicle{
-    public Bakkie(String color, String vinNumber, String model, int year, double volume) {
+public class Bakkie extends Vehicle
+{
+    //Bakkie Class Constructor
+    public Bakkie(String color, String vinNumber, String model, int year, double volume) 
+    {
         super(color, vinNumber, model, year, volume);
         this.price = 150;
     }
 
-
     @Override
-    public double GetPrice(CustomerSlip slip) {
+    public double GetPrice(CustomerSlip slip) 
+    {
         return this.price * slip.GetKilometres();
     }
 }
